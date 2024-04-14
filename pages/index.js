5a61f1a9ff2bbc521d689502eb4dcbc511e15400
william <williamlibero@profkenny.com.br>
2024-04-13 23:21:21 -0300

    Responsivity bugs fix.

--- pages/index.js
+++ pages/index.js
@@ -70,7 +70,7 @@ export default function Home(props) {
                   href={individualNew.url}
                   target="_blank"
                 >
-                  Read more <LuArrowUpRightSquare />
+                  <span>Read more</span> <LuArrowUpRightSquare />
                 </a>
               </div>
             </div>
